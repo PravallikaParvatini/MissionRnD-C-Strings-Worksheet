864925f54bda645cd3808c133e8dacc01895f43d
PravallikaParvatini <PravallikaParvatini@missionrnd.com>
2016-03-18 21:25:26 +0530

    Submit Commit

--- src/commonWords.cpp
+++ src/commonWords.cpp
@@ -18,8 +18,53 @@ NOTES: If there are no common words return NULL.
 #define SIZE 31
 
 char ** commonWords(char *str1, char *str2) {
-	char **str;
+	char **str=NULL;
+	int i = 0, j = 0, k = 0, m = 0, count = 0,count1=0,l=0;
 	if (str1 == NULL || str2 == NULL)
 		return NULL;
-
+	str = (char **)malloc(0*sizeof(char *));
+	while (str1[i] != '\0')
+	{
+		k = i;
+		while (str1[i] == str2[j] && str1[i] !=' ')
+		{
+			i++;
+			j++;
+		}
+		if ((str1[i] == ' '||str1[i]=='\0')&&(str2[j] == ' '||str2[j]=='\0')&&i!=0&&j!=0)
+		{
+			count1++;
+			str = (char **)realloc(str, count1*sizeof(char *));
+			m = 0;
+			str[count] = (char *)malloc(SIZE*sizeof(str));
+			for (l = k; l < i; l++)
+			{
+				str[count][m] = str1[l];
+				m++;
+			}
+			str[count][m] = '\0';
+			count++;
+			i++;
+			j = 0;
+		}
+		else
+		{
+			while (str2[j] != ' ' && str2[j] != '\0')
+				j++;
+			if (str2[j] == '\0')
+			{
+				j = 0;
+				while (str1[i] != ' ' && str1[i] != '\0')
+					i++;
+				if (str1[i] != '\0')
+					i++;
+			}
+			else
+				j++;
+		}
+	}
+	if (count1 == 0)
+		return NULL;
+	else
+		return str;
 }
